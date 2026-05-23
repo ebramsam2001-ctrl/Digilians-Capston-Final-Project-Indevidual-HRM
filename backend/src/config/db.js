37a28b76e2ee0b2c9b7de97d16222438626c8624
“Ebram_Samuel” <ebramsam2001@gmail.com>
2026-05-23 12:21:32 +0300

    make configration first step

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,37 @@
+// strict mood
+"use strict";
+
+// mangoose
 const mongoose = require("mongoose");
 
+// in dev mood we want to get the error quickly
+// makes queries fail immediately if the DB is not connected
+mongoose.set(`bufferCommands`, false);
+
+// connect to database
 const connectDB = async () => {
+    const url = process.env.MONGO_URI;
+
+    if(!url) {
+        throw new Error(`MONGO_URI is not defined in .env`);
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("☑️ DB connected");
+        const connected = await mongoose.connect(url, {
+            maxPoolSize: 10, // make 10 chanals to request pressure handling and speedly
+            serverSelectionTimeoutMS: 5000, // the connection methode will wait max: 5 Sec to not wait forever
+            dbName:`HRManagement`,
+        });
+
+        console.log(`MongoDB connected: ${connected.connection.host}`);
     } catch (error) {
-        console.log(`🌋 ${error}`);
+        console.error(`MongoDB error: ${error.message}`);
+
+        // close the backend with error
+        // close the app if the database connection faild
+        process.exit(1);
     }
 };
 
+// exporting
 module.exports = connectDB;
